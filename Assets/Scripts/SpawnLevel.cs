39e29440b7390c744227fd52fc938e7ccb874774
Evan Tichenor <evan.tichenor@gmail.com>
2019-04-13 10:39:37 -0400

    SampleScene now has a simple player and camera with the level creator

--- Assets/Scripts/SpawnLevel.cs
+++ Assets/Scripts/SpawnLevel.cs
@@ -28,6 +28,11 @@ public class SpawnLevel : MonoBehaviour {
     [SerializeField]
     private int seed;
 
+    [SerializeField]
+    private int amountOfStuff;
+    [SerializeField]
+    private int patchAmount;
+
     /// <summary>
     /// Spawns the level.
     /// </summary>
@@ -35,7 +40,7 @@ public class SpawnLevel : MonoBehaviour {
         terrain = level.terrainData;
         Random.InitState(seed);
 
-        for(int i = 0; i < 25; i++) {
+        for(int i = 0; i < amountOfStuff; i++) {
             SpawnOrePatch();
             SpawnRocks();
             SpawnBoulders();
@@ -58,7 +63,7 @@ public class SpawnLevel : MonoBehaviour {
         int x = 0, y = 0;
         MakeXY(ref x, ref y);
 
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < patchAmount; i++) {
             int newX = x + Random.Range(-5, 5), newY = y + Random.Range(-5, 5);
             float height = terrain.GetHeight(newX, newY);
             GameObject.Instantiate(obj, new Vector3(newX, height, newY), Quaternion.identity);
