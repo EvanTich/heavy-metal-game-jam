ee1af1b40c8792cd502bc9d05342c63ac1d37cd4
Linsey Briley <ljb26675@uga.edu>
2019-04-12 21:38:24 -0400

    added simple payer movement script in simpleplayercontrols scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    Rigidbody rb;
+    public float speed = 10.0f;
+
+
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
     }
 
     // Update is called once per frame
     void Update() {
-        
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
     }
 }
