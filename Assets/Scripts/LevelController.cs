673fde4c4c72eccb42e148813e16a1a85f8ec03d
Evan Tichenor <evan.tichenor@gmail.com>
2019-04-14 11:18:39 -0400

    changed level controller to use multiple players instead of just one

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -15,7 +15,7 @@ public class LevelController : MonoBehaviour {
     public static int[] OreAmounts { get; private set; }
 
     [SerializeField]
-    private GameObject player;
+    private GameObject[] players;
 
     static LevelController() {
         Timer = 6000;
@@ -68,7 +68,7 @@ public class LevelController : MonoBehaviour {
             Names[i] = names[i];
 
             var obj = GameObject.Instantiate(
-                player, 
+                players[i], 
                 new Vector3(3 * Mathf.Cos(curr) + transform.position.x, transform.position.y + 5, 3 * Mathf.Sin(curr) + transform.position.z), 
                 Quaternion.Euler(0, curr, 0)
             );
