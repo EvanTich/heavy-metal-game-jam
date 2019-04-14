d4abf874a1eef75a86a851940b677c7ddc0d1c33
Linsey Briley <ljb26675@uga.edu>
2019-04-14 00:08:28 -0400

    added mole that moves to prefab

--- Assets/Scripts/AttractOre.cs
+++ Assets/Scripts/AttractOre.cs
@@ -24,24 +24,8 @@ public class AttractOre : MonoBehaviour {
             if (ore != null)
             {
                 Vector3 direction = transform.position - ore.transform.position;
-                //Debug.Log(direction);
                 ore.gameObject.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * direction);
-                //Debug.Log(strengthOfAttraction + " " + direction);
             }
-        }
-        if (insidePlayer)
-        {
-            if (other != null)
-            {
-                Vector3 direction = transform.position - other.transform.position;
-                //Debug.Log("NEXT TO A PLAYER!!");
-                other.gameObject.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * direction);
-                //Debug.Log(strengthOfAttraction + " " + direction);
-
-                Vector3 direction2 = other.transform.position - transform.position;
-                gameObject.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * direction2);
-            }
-
         }
     }
 
@@ -52,16 +36,6 @@ public class AttractOre : MonoBehaviour {
             inside = true;
             this.ore = other.gameObject;
         }
-        if(other.gameObject.tag == "Player")
-        {
-            //make them stick together
-            // Debug.Log("debug");
-            //Debug.Log("NEXT TO A PLAYER!!");
-            insidePlayer = true;
-            this.other = other.gameObject;
-        }
- 
-       //ebug.Log(other.gameObject.name);
     }
 
     void OnTriggerExit(Collider other)
@@ -71,21 +45,7 @@ public class AttractOre : MonoBehaviour {
             inside = false;
             this.ore = null;
         }
-        if (other.gameObject.tag == "Player")
-        {
-            insidePlayer = false;
-            this.other = null;
-        }
+       
     }
 
-/*    private void OnTriggerStay(Collider other)
-    {
-
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("HIT A PLAYER!!!!!");
-        }8/
-
-        //Debug.Log("Test");
-    }*/
 }
